6301e21c9b8cfe15ee6af3011006bc32f5072fb0
Jack_PC <jfleming48@my.bcit.ca>
2025-03-03 11:16:08 -0800

    First class learning JS

---
+++ week_7/index.js
@@ -0,0 +1,22 @@
+// let scoops = 5;
+// while (scoops > 0) {
+//     document.write("Another Scoop<br>");
+//     if (scoops < 3) {
+//     alert("Ice cream is running low!");
+//     } else if (scoops >= 5) {
+//     alert("Eat faster, the ice cream is going to melt!");
+//     }
+//     scoops = scoops - 1;
+// }
+// document.write("Life isn't the same without ice cream!")
+
+// let a = 10;
+// a = "john";
+// console.log(a);
+
+// console.log(Math.random(1,7)); // 0 inclusive to 1 exclusive
+//0.775255
+//0.2523525
+//0.119078
+
+console.log(Math.floor(Math.random()*100));
